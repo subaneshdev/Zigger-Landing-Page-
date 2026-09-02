746d321684004e5975952cc1e60d5ec76cec893c
vijaykumarunfounded-ctrl <vijaykumarunfounded@gmail.com>
2026-09-02 19:41:39 +0530

    feat(seo): configure automated Next.js sitemap.xml and robots.txt routes

---
+++ src/app/robots.js
@@ -0,0 +1,12 @@
+﻿export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/partner/dashboard'],
+      },
+    ],
+    sitemap: 'https://www.ziggers.in/sitemap.xml',
+  };
+}
